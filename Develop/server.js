a4d6e51d983bb2d127fc55a93463f3ed9ca34011
Myles Nichols <mylesnichols21@yahoo.com>
2023-03-10 23:42:12 -0500

    restructured files

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require('path');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
